0b0817af10ac92c5e1f128cacbbde30cb6c50845
telloviz <lollisjosh@csu.fullerton.edu>
2024-10-04 08:33:53 -0700

    scripts.js styles.css

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Load the footer
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-placeholder").innerHTML = data;
+        });
+});
