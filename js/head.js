7f99675500bc0d28344813247812a806d813858f
TelloViz <lollisjosh@csu.fullerton.edu>
2026-02-22 19:01:35 -0800

    Fixed header list hover action

--- js/head.js
+++ js/head.js
@@ -85,6 +85,7 @@
     const fontAwesome = appendLink({
       rel: "stylesheet",
       href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css",
+      crossorigin: "",
     });
 
     // Site CSS stack
